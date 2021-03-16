3c94659ea9822f731f86083b6bd8ed59c6ac4865
Anonimo117 <alangarciac0516@gmail.com>
2021-03-15 19:47:50 -0600

    PruebaTarea

---
+++ Tarea_Alumno.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebas;
+
+/**
+ *
+ * @author Alan
+ */
+public class Tarea_Alumno {
+    
+    public double base;
+   public double altura;
+   
+   public Tarea_Alumno(double b, double a){
+       this.base = b;
+       this.altura=a;
+   }
+   
+   // ejemplo de método con errores
+   public int mul(int a, int b){
+       int c = 0;
+       int cont = 0;
+       cont++;
+       
+       while(cont<10){
+           c = c+(a*b);
+       }
+       return c;
+   }
+   
+   public double calculaperimetro(){
+       double perimetro;
+       perimetro = this.altura*this.base;
+       return perimetro;
+   }
+    
+}
